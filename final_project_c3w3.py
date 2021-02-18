129079d88d20bf784410a91da4de531ccf1643eb
fergmack <33000542+fergmack@users.noreply.github.com>
2021-02-18 19:02:07 +0000

    Update final_project_c3w3.py

--- final_project_c3w3.py
+++ final_project_c3w3.py
@@ -1,3 +1,5 @@
+
+
 # ------------ Part 1
 import time 
 
@@ -25,3 +27,43 @@ myString = "Hellow, World! 123"
 
 print(myString.upper())
 print(myString.lower())
+print(myString.count('l'))
+
+s = 'python is pythonic'
+print(s.count('python'))
+
+# We’re going to define a few useful methods for you:
+# * getNumberBetween(prompt, min, max)) repeatedly asks the user for a number between min and max with the prompt prompt
+
+# * spinWheel() simulates spinning the wheel and returns a dictionary with a random prize
+
+# * getRandomCategoryAndPhrase() returns a tuple with a random category and phrase for players to guess
+
+# * obscurePhrase(phrase, guessed) returns a tuple with a random category and phrase for players to guess
+
+import json 
+import random
+import time
+
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+# Repeatedly asks the user for a number between min & max (inclusive)
+def getNumberBetween(prompt, min, max):
+  userinp = input(prompt) # ask the first time
+  while True:
+    try:
+      n = int(userinp) # try casting to an integer
+      if n < min:
+        errmessage = 'Must be at least {}'.format(min)
+      elif n > max:
+        errrmessage = 'Must be less than {}'.format(max)
+      else:
+        return n
+    except ValueError: # the user didn't enter a number
+      errmessage = '{} is not a number.'.format(userinp)
+
+    # If we haven't gotten a number yet, add the eorror message and ask again
+    userinp = input('{}\n{}'.format(errmessage, prompt))
+
+getNumberBetween('Enter number between 1 and 10', 1, 10)
+
